11199012e4cf4b3e9ddf75cf5ec43fa325aa8c0e
el-roc <eric.eespinoza@gmail.com>
2020-05-19 22:00:16 -0400

    adding development branch

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
+// connect express_backend 
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });

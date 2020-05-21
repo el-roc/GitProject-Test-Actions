59e77028ac903f8a56b333ca39a557e355434021
el-roc <eric.eespinoza@gmail.com>
2020-05-21 12:32:20 -0400

    reverting get to express-backend

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -15,7 +15,7 @@ state = {
   }
     // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
   callBackendAPI = async () => {
-    const response = await fetch('/');
+    const response = await fetch('/express_backend');
     const body = await response.json();
 
     if (response.status !== 200) {

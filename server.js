d172f6c26820eb7838f5df92e26c310da8afdc51
el-roc <eric.eespinoza@gmail.com>
2020-05-21 14:08:11 -0400

    added static filepath server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
+////
+app.use(express.static(path.join(__dirname, 'react-ui/public')));
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
